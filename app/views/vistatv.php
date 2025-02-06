8a3f82b9339c696000c6889b981ef23efa9aee5d
Arley-Anacona-Anacona <arleyanacona2017@gmail.com>
2025-02-06 15:22:46 -0500

    url de peticion actualizada a la ip del servidor

--- app/views/vistatv.php
+++ app/views/vistatv.php
@@ -89,7 +89,7 @@ require_once("../templates/boostrap.php");
 </div>
 
 <!-- <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script> -->
-<script src="../../public/js/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<script src="../../public/js/jquery-3.5.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 <script src="../turno.js"></script>
@@ -125,7 +125,7 @@ require_once("../templates/boostrap.php");
     function hacerpeticion() {
       $.ajax({
         type: "GET",
-        url: "http://localhost/turnos_dcmy/app/controllers/obtener_turnos.php",
+        url: "http://192.168.1.2/turnos_dcmy/app/controllers/obtener_turnos.php",
         dataType: "json",
         success: function(data) {
           $("#ultimo").text(data.ultimo);
